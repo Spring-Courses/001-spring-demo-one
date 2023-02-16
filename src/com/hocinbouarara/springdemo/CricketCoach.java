0b751e7ab2c9a897a287e695573e9ac52eb0e57f
hocinebouarara <hocine@univ-tiaret.dz>
2023-02-16 15:05:30 +0100

    Dependency injection (DI) using values injection and Xml configuration

--- src/com/hocinbouarara/springdemo/CricketCoach.java
+++ src/com/hocinbouarara/springdemo/CricketCoach.java
@@ -3,6 +3,12 @@ package com.hocinbouarara.springdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	// add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
+	
 
 	// creat a no-arg constructor
 	public CricketCoach() {
@@ -13,6 +19,26 @@ public class CricketCoach implements Coach {
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
+	}
+	
+	
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
 	}
 
 	@Override
