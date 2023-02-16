11839e4329c3a4bcf25e8cea08a84376e18c85fa
hocinebouarara <hocine@univ-tiaret.dz>
2023-02-16 15:45:12 +0100

    bean scopes (singleton/prototype ....)

---
+++ src/com/hocinbouarara/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,34 @@
+package com.hocinbouarara.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context=
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+
+		Coach alphaCoach = context.getBean("myCoach",Coach.class);
+		
+		// check if they are the same 
+		boolean result = (theCoach == alphaCoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object "+result);
+		
+		System.out.println("\nMemory location for theCoach: "+theCoach);
+		System.out.println("\nMemory location for alphaCoach: "+alphaCoach);
+		
+		// close the context
+		context.close();
+
+
+	}
+
+}
